4514abe814a1edd8404ac7bff8c3f6629e71598c
KhallilB <khallil.bailey@gmail.com>
2019-10-04 07:48:10 -0700

    [add] Eslint & editor config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: 'airbnb',
+  rules: {
+    // windows linebreaks when not in production environment
+    'linebreak-style': ['error', process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'],
+  },
+  parser: 'babel-eslint',
 };
