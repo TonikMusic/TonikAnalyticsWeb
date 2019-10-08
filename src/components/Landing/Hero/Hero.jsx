7138f6c4e67e284759d6b72b5d45e7ffa86a9369
KhallilB <khallil.bailey@gmail.com>
2019-10-08 15:49:14 -0700

    [add]  landing page

--- src/components/Landing/Hero/Hero.jsx
+++ src/components/Landing/Hero/Hero.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Hero.css';
+import Iphones from '../../../assets/images/Iphone_Pictures.png';
 
 function Hero() {
   return (
-    <div>
-      <h1>Hero Component</h1>
+    <div className="hero__container">
+      <div className="hero__background_image" />
+
+      <div id="hero__two_columns">
+        <div className="hero__column_left">
+          <h1>Become the artist we want you to be</h1>
+          <p>Take Control Of Your Internet Presence</p>
+        </div>
+        <div className="hero__column_right">
+          <p>All your stats in one place</p>
+          <img src={Iphones} alt="#" />
+        </div>
+      </div>
     </div>
   );
 }
