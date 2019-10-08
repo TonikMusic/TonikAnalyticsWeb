8b2e69512cd3ac9c2303747e0c6f5bb5f769a430
KhallilB <khallil.bailey@gmail.com>
2019-10-08 11:17:14 -0700

    [add] starting hero componenet

---
+++ src/components/Landing/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <div>
+      <h1>Hero Component</h1>
+    </div>
+  );
+}
+
+export default Hero;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Landing.css';
 
+import Hero from './Hero/Hero';
+
 function Landing() {
   return (
     <div>
-      <h1>Landing Works</h1>
+      <Hero />
     </div>
   );
 }
