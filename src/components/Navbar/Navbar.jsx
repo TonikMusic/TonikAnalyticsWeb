3b3ac0795badb5d0d8fdee42f02a37d2723f3aa2
KhallilB <khallil.bailey@gmail.com>
2019-10-08 23:13:11 -0700

    [add] react-router

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css';
+
+class Navbar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Navbar Works</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
