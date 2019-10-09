2041bc6169e1d0294d755c40c6d330b0d345d1b0
KhallilB <khallil.bailey@gmail.com>
2019-10-08 21:16:23 -0700

    [add] Landing page

--- src/components/Landing/Footer/Footer.jsx
+++ src/components/Landing/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import Logo from '../../../assets/logos/FullNoColor.png';
+import IOSDownload from '../../../assets/images/DownloadOnIOS.png';
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer Works</h1>
+    <div className="footer__container">
+      <div id="footer__two_columns">
+        <div className="footer__left_column">
+          <img src={Logo} alt="#" />
+          <p>&copy; 2019 Tonik</p>
+        </div>
+        <div className="footer__right_column">
+          <p>Try Our Mobile App</p>
+          <img src={IOSDownload} alt="#" />
+        </div>
+      </div>
     </div>
   );
 }
