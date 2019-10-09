a310d6b2614150bc6e2b7348348b55fc4ae71369
KhallilB <khallil.bailey@gmail.com>
2019-10-09 11:19:37 -0700

    [add] signup form

--- src/components/auth/SignUp/Signup.jsx
+++ src/components/auth/SignUp/Signup.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Signup.css';
-
-import Logo from '../../../assets/logos/FullNoColor.png';
+import { Link } from 'react-router-dom';
 
 class Signup extends React.Component {
   constructor() {
@@ -13,16 +12,22 @@ class Signup extends React.Component {
   render() {
     return (
       <div className="signup__container">
-        <img src={Logo} alt="#" />
         <form>
           <h1>Sign Up</h1>
 
           <input type="text" placeholder="Full Name" />
           <input type="text" placeholder="Username" />
+          <input type="date" placeholder="Date of Birth" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
+          <p>
+            Already a member?
+            <span>
+              <Link to="/login"> Log In</Link>
+            </span>
+          </p>
 
-          <button type="submit">Signup</button>
+          <button type="submit">Get Started</button>
         </form>
       </div>
     );
