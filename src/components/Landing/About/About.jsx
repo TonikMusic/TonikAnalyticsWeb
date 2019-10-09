08ef07d51010f1be63f4d482a2d72374cab0d8c2
KhallilB <khallil.bailey@gmail.com>
2019-10-08 17:16:36 -0700

    [add] About component

---
+++ src/components/Landing/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <h1>About Works</h1>
+    </div>
+  );
+}
+
+export default About;
