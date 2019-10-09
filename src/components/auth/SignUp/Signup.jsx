b5620472042bee5c4341b461e114c9c23a184d1c
KhallilB <khallil.bailey@gmail.com>
2019-10-09 10:20:16 -0700

    [add] input fields

--- src/components/auth/SignUp/Signup.jsx
+++ src/components/auth/SignUp/Signup.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Signup.css';
 
+import Logo from '../../../assets/logos/FullNoColor.png';
+
 class Signup extends React.Component {
   constructor() {
     super();
@@ -11,7 +13,17 @@ class Signup extends React.Component {
   render() {
     return (
       <div className="signup__container">
-        <h1>Sign Up</h1>
+        <img src={Logo} alt="#" />
+        <form>
+          <h1>Sign Up</h1>
+
+          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+
+          <button type="submit">Signup</button>
+        </form>
       </div>
     );
   }
