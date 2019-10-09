5555499c7d2d926a9058566fc24bfe39e3c0e3ec
KhallilB <khallil.bailey@gmail.com>
2019-10-08 17:53:26 -0700

    [add] Footer component

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,12 +3,14 @@ import './Landing.css';
 
 import Hero from './Hero/Hero';
 import About from './About/About';
+import Footer from './Footer/Footer';
 
 function Landing() {
   return (
     <div>
       <Hero />
       <About />
+      <Footer />
     </div>
   );
 }
