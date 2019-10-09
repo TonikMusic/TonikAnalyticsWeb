6dfc0a21a7a3e9a1f366070ee0adab5621519cbc
KhallilB <khallil.bailey@gmail.com>
2019-10-08 17:49:52 -0700

    [add] Styled about section

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './Landing.css';
 
 import Hero from './Hero/Hero';
+import About from './About/About';
 
 function Landing() {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 }
