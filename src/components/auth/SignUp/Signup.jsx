ef0c40b4f485a61e825301f539fe3d1f4b57c471
KhallilB <khallil.bailey@gmail.com>
2019-10-09 15:27:40 -0700

    setting up redux

--- src/components/auth/SignUp/Signup.jsx
+++ src/components/auth/SignUp/Signup.jsx
@@ -6,20 +6,72 @@ class Signup extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      fullName: '',
+      username: '',
+      dateOfBirth: '',
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(e.target.value);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
   render() {
+    const {
+      fullName, username, dateOfBirth, email, password,
+    } = this.state;
+
     return (
       <div className="signup__container">
         <form>
           <h1>Sign Up</h1>
 
-          <input type="text" placeholder="Full Name" />
-          <input type="text" placeholder="Username" />
-          <input type="date" placeholder="Date of Birth" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input
+            name="fullName"
+            type="text"
+            placeholder="Full Name"
+            value={fullName}
+            onChange={this.handleChange}
+          />
+          <input
+            name="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={this.handleChange}
+          />
+          <input
+            name="dateOfBirth"
+            type="date"
+            placeholder="Date of Birth"
+            value={dateOfBirth}
+            onChange={this.handleChange}
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={this.handleChange}
+          />
           <p>
             Already a member?
             <span>
