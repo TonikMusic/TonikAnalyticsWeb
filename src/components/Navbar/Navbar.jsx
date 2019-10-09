852ee0f4d122c2691f2914e10fd69bc5b17c6ca9
KhallilB <khallil.bailey@gmail.com>
2019-10-09 00:01:46 -0700

    [add] Navbar showing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Navbar.css';
 
+import Logo from '../../assets/logos/SingleColor.png';
+
 class Navbar extends React.Component {
   constructor() {
     super();
@@ -10,8 +12,28 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Navbar Works</h1>
+      <div className="navbar__container">
+        <div>
+          <img src={Logo} alt="#" />
+        </div>
+
+        <ul>
+          <li className="navbar__item">
+            News
+          </li>
+          <li className="navbar__item">
+            Get Started
+          </li>
+        </ul>
+
+        <ul>
+          <li className="navbar__item">
+            Log In
+          </li>
+          <li className="navbar__item">
+            Sign Up
+          </li>
+        </ul>
       </div>
     );
   }
