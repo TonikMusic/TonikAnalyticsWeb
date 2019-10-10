bc3ad4ac04ac2d78db4d5b692f772346e173588c
KhallilB <khallil.bailey@gmail.com>
2019-10-10 04:16:02 -0700

    [add] Signup service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,31 @@
-import {
-  authHeader
-} from '../helpers/auth-header';
+/* eslint-disable no-undef */
+// import {
+//   authHeader
+// } from '../helpers/auth-header';
+
+// Remove user-token from local storage
+function logout() {
+  localStorage.removeItem('user');
+}
+
+function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // auto logout if 401 response returned from api
+        logout();
+        // eslint-disable-next-line no-restricted-globals
+        location.reload(true);
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
 
 // Logs a registered user in
 function login(email, password) {
@@ -8,7 +33,7 @@ function login(email, password) {
   const requestOptions = {
     method: 'POST ',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       email,
@@ -18,6 +43,7 @@ function login(email, password) {
 
   // Get the user via the backend route
   return fetch(`${process.env.local.API_URL}/login`, requestOptions)
+    .then(handleResponse)
     .then((user) => {
       localStorage.setItem('user', JSON.stringify(user));
 
@@ -25,12 +51,20 @@ function login(email, password) {
     });
 }
 
-function logout() {
-  // Remove user-token from local storage
-  localStorage.removeItem('user');
-}
+// Registers user based on Form Data
+function signup(user) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user),
+  };
 
-function signup() {}
+  // Get the user via the backend route
+  return fetch(`${process.env.local.API_URL}/signup`, requestOptions)
+    .then(handleResponse);
+}
 
 export default {
   login,
